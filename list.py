162da007a1243e4728afa1da969c9b019cd4a178
jaloliddin05 <nasrullayevjaloliddin.ajf@gmail.com>
2023-11-13 16:50:58 +0500

    lists

---
+++ list.py
@@ -0,0 +1,26 @@
+# Lists
+
+a = [1,2,3,4]
+b = ["as",'bdsc', 'hello']
+#
+print(a[0])
+print(len(a))
+#
+#n = int(input("Enter number: "))
+#print(n)
+#
+print("  1 2 3 4 5667  ".strip())
+#
+a.insert(2,"lala")
+print(a)
+#
+print(a.count(1))
+#
+print(a.index(3))
+#
+print(a.pop())
+print(a.pop(1))
+print(a)
+#
+a.clear()
+print(a)
