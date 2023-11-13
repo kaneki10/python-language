dfa717242451e02414f96b5beb54c6e023a3d9a3
jaloliddin05 <nasrullayevjaloliddin.ajf@gmail.com>
2023-11-13 13:11:22 +0500

    operators

---
+++ operators.py
@@ -0,0 +1,69 @@
+# Arithmetic operators
+'''
++  
+-
+*
+/   ---  print(19/4)  -> 4.75  float
+//  ---  print(19//4) -> 4     floor
+%   ---  print(19%4)  -> 3     remainder
+**  ---  print(4**4)  -> 256   power
+'''
+
+# Relational operators
+'''
+>
+<
+==
+!=
+>=
+<=
+'''
+
+# Bitwise operators
+'''
+&
+|
+^
+~
+>>
+<<
+'''
+
+# Logical operators
+'''
+and
+or
+not
+'''
+
+# Assigment operators
+'''
+=
++=
+-=
+*=
+/=
+%=
+//=
+**=
+&=
+|=
+^=
+>>=
+<<=
+'''
+
+# Compound operators
+'''
+as Assigment
+'''
+
+# Membership operators
+'''
+in
+'''
+
+# Identity operators
+'''
+is
+'''
