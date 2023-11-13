672035c139235cfc10406da1cec893aab68b1497
jaloliddin05 <nasrullayevjaloliddin.ajf@gmail.com>
2023-11-13 13:21:47 +0500

    condition

---
+++ condition.py
@@ -0,0 +1,21 @@
+# Conditional statements
+
+# if else, elif
+
+
+a = 15
+b = 25
+
+if a > b:
+    print("la")
+elif b > a:
+    print("ha")
+else:
+    print("ye")
+
+
+# if else ternary
+
+min = a if a < b else b 
+print(min)
+
