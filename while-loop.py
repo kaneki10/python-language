6ca9e99bd8993517644d387415ed85b927335434
jaloliddin05 <nasrullayevjaloliddin.ajf@gmail.com>
2023-11-13 14:12:43 +0500

    while loop

---
+++ while-loop.py
@@ -0,0 +1,18 @@
+# While loop
+
+# While loop logic building
+
+a = 10
+
+while a > 1:
+    a -= 1
+    if a == 5: continue
+    elif a == 3: break
+    print(a)
+
+while a > 1:
+    if(a == 7): pass
+    else: print(a)
+    a -= 1
+else:
+    print("lala")
